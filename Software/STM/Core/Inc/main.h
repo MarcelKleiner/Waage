c4db712ac75966db39dba119ee4508a5934b20e3
marcelkleiner <marcel.kleiner@hotmail.com>
2020-10-09 08:41:55 +0200

    update layout

--- Software/STM/Core/Inc/main.h
+++ Software/STM/Core/Inc/main.h
@@ -58,6 +58,40 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LCD_RS_Pin GPIO_PIN_14
+#define LCD_RS_GPIO_Port GPIOC
+#define LCD_RW_Pin GPIO_PIN_15
+#define LCD_RW_GPIO_Port GPIOC
+#define LCE_E_Pin GPIO_PIN_0
+#define LCE_E_GPIO_Port GPIOA
+#define DB0_Pin GPIO_PIN_1
+#define DB0_GPIO_Port GPIOA
+#define DB1_Pin GPIO_PIN_2
+#define DB1_GPIO_Port GPIOA
+#define SW_BW_Pin GPIO_PIN_3
+#define SW_BW_GPIO_Port GPIOA
+#define LED_BW_Pin GPIO_PIN_4
+#define LED_BW_GPIO_Port GPIOA
+#define SW_PW_Pin GPIO_PIN_5
+#define SW_PW_GPIO_Port GPIOA
+#define LED_PW_Pin GPIO_PIN_6
+#define LED_PW_GPIO_Port GPIOA
+#define SW_SUM_Pin GPIO_PIN_7
+#define SW_SUM_GPIO_Port GPIOA
+#define LED_SUM_Pin GPIO_PIN_0
+#define LED_SUM_GPIO_Port GPIOB
+#define SW_TOTAL_Pin GPIO_PIN_1
+#define SW_TOTAL_GPIO_Port GPIOB
+#define SW_TAR_Pin GPIO_PIN_8
+#define SW_TAR_GPIO_Port GPIOA
+#define DB3_Pin GPIO_PIN_15
+#define DB3_GPIO_Port GPIOA
+#define LC2_CS_Pin GPIO_PIN_6
+#define LC2_CS_GPIO_Port GPIOB
+#define LC1_CS_Pin GPIO_PIN_7
+#define LC1_CS_GPIO_Port GPIOB
+#define DB2_Pin GPIO_PIN_3
+#define DB2_GPIO_Port GPIOH
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Software/STM/Core/Src/main.cpp
+++ Software/STM/Core/Src/main.cpp
@@ -19,6 +19,10 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
+#include "spi.h"
+#include "usb_device.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -83,6 +87,10 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_SPI1_Init();
+  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -106,6 +114,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Configure the main internal regulator output voltage
   */
@@ -116,11 +125,18 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = 0;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_11;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -129,12 +145,19 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1|RCC_PERIPHCLK_USB;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_MSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
